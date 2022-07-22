c2150eeeecb4d3f2583566bf51edd192ea99194c
Joy096 <44851817+Joy096@users.noreply.github.com>
2022-07-22 21:57:54 +0300

    Add files via upload

---
+++ server.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sudo cp /home/ubuntu/.ssh/authorized_keys /root/.ssh/
+sudo chown -R root. /root/.ssh/
+sudo apt update
+sudo apt full-upgrade -y
+yes Y | sudo apt autoremove
+sudo systemctl stop netfilter-persistent
+sudo systemctl disable netfilter-persistent
+sudo ufw default deny incoming
+sudo ufw default allow outgoing
+sudo ufw allow 22
+sudo ufw allow 80
+sudo ufw allow 443
+yes Y | sudo ufw enable
+
