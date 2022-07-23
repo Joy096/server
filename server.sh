41efcd08332225ae1d65049f589c48a7fdb5a485
Joy096 <44851817+Joy096@users.noreply.github.com>
2022-07-24 00:52:42 +0300

    Update server.sh

--- server.sh
+++ server.sh
@@ -17,4 +17,32 @@ wget -N --no-check-certificate https://github.com/teddysun/across/raw/master/bbr
 chmod +x bbr.sh
 sudo bash bbr.sh
 sudo dpkg-reconfigure tzdata
+sudo wget -P /root/ https://raw.githubusercontent.com/Joy096/server/main/updater.sh
+sudo chmod +x /root/updater.sh
+sudo echo -e "# Edit this file to introduce tasks to be run by cron.
+#
+# Each task to run has to be defined through a single line
+# indicating with different fields when the task will be run
+# and what command to run for the task
+#
+# To define the time you can provide concrete values for
+# minute (m), hour (h), day of month (dom), month (mon),
+# and day of week (dow) or use '*' in these fields (for 'any').
+#
+# Notice that tasks will be started based on the cron's system
+# daemon's notion of time and timezones.
+#
+# Output of the crontab jobs (including errors) is sent through
+# email to the user the crontab file belongs to (unless redirected).
+#
+# For example, you can run a backup of all your user accounts
+# at 5 a.m every week with:
+# 0 5 * * 1 tar -zcf /var/backups/home.tgz /home/
+#
+# For more information see the manual pages of crontab(5) and cron(8)
+#
+# m h  dom mon dow   command
+0 4 * * * bash /root/updater.sh > /root/updater.log" | sudo crontab -
+sudo /etc/init.d/cron restart
+
 sudo reboot
