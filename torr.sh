1c1f98f59f692c212a4661740d414fbffc090977
Joy096 <44851817+Joy096@users.noreply.github.com>
2023-10-22 18:24:12 +0300

    Update torr.sh

--- torr.sh
+++ torr.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-wget https://github.com/YouROK/TorrServer/releases/latest/download/TorrServer-linux-arm64
-mv TorrServer-linux-arm64 /opt/torrserver
-chmod +x /opt/torrserver/TorrServer-linux-arm64
-systemctl restart torrserver
+bash <(curl -Ls https://raw.githubusercontent.com/YouROK/TorrServer/master/installTorrServerLinux.sh) --update
