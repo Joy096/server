a71f18aded0a7c3ba19a20e889982e5c4efa54fb
Joy096 <44851817+Joy096@users.noreply.github.com>
2023-10-26 09:50:50 +0300

    Update server.sh

--- server.sh
+++ server.sh
@@ -4,6 +4,8 @@ sudo cp /home/ubuntu/.ssh/authorized_keys /root/.ssh/
 sudo chown -R root. /root/.ssh/
 sudo apt update
 sudo apt full-upgrade -y
+sudo apt install aptitude -y
+sudo aptitude upgrade -y
 yes Y | sudo apt autoremove
 sudo systemctl stop netfilter-persistent
 sudo systemctl disable netfilter-persistent
