1feabe2eb1e70d3d1cb9c94089264d0d12e0819b
Joy096 <44851817+Joy096@users.noreply.github.com>
2024-09-25 08:08:53 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,43 @@
+
 #!/bin/bash
 
+# Обновление системы
+sudo apt update
+sudo apt upgrade -y
+sudo apt autoremove -y
+
+# Настройка брандмауэра 
+sudo ufw default deny incoming
+sudo ufw default allow outgoing
+sudo ufw allow 22/tcp # Разрешить SSH
+sudo ufw allow 80/tcp  # Разрешить HTTP
+sudo ufw allow 443/tcp # Разрешить HTTPS
+sudo ufw --force enable
+
+# Настройка сети
+echo -e "net.core.default_qdisc=fq\nnet.ipv4.tcp_congestion_control=bbr" | sudo tee --append /etc/sysctl.conf
+sudo sysctl -p
+
+# Настройка локали 
 sudo locale-gen ru_UA.utf8
 sudo update-locale LANG=ru_UA.UTF8
-locale | sudo tee /etc/default/locale
+sudo dpkg-reconfigure locales
+
+# Настройка часового пояса
+sudo timedatectl set-timezone Europe/Kiev
+
+# Скачивание и настройка скрипта updater.sh 
+sudo wget -P /root/ https://raw.githubusercontent.com/Joy096/server/main/updater.sh
+sudo chmod +x /root/updater.sh
+
+# Добавление задания в cron для пользователя root
+(sudo crontab -u root -l 2>/dev/null; echo "0 4 * * * bash /root/updater.sh > /var/log/updater.log") | sudo crontab -u root -
+
+# Перезапуск службы cron
+sudo systemctl restart cron
+
+# Установка aptitude 
+sudo apt install aptitude -y
+
+# Обновление пакетов с помощью aptitude 
+sudo aptitude upgrade -y
