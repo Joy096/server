300a09c18a2d935103c46ca81a27b3f81fad737e
Joy096 <44851817+Joy096@users.noreply.github.com>
2024-09-27 15:17:32 +0300

    Update outline_vpn.sh

--- outline_vpn.sh
+++ outline_vpn.sh
@@ -5,6 +5,13 @@ declare management_port=""
 declare access_key_port=""
 declare api_info=""
 
+# Переменная для зеленого цвета и сброса цвета
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+# Путь к файлу для хранения портов и API URL
+CONFIG_FILE="/root/outline_data"
+
 function install_outline {
     echo "Установка Docker..."
     curl -fsSL https://get.docker.com/ | sh
@@ -13,23 +20,18 @@ function install_outline {
     # Сохраняем весь вывод установки в переменную install_output
     install_output=$(yes Y | SB_IMAGE=oreoluwa/shadowbox:daily sudo --preserve-env bash -c "$(curl -Ls https://raw.githubusercontent.com/EricQmore/installer/main/install_server.sh)" install_server.sh 2>&1)
 
-    # Извлечение строки с API URL
-    api_info=$(echo "$install_output" | grep -oP '"apiUrl":"https://[^"]+"')
+    # Извлечение строки с API URL и сертификатом
+    api_info=$(echo "$install_output" | grep -oP '{"apiUrl":"https://.*?","certSha256":"[a-fA-F0-9]{64}"}')
 
     # Извлечение портов для управления и ключей доступа
     management_port=$(echo "$install_output" | grep -oP '(?<=Management port )\d+')
     access_key_port=$(echo "$install_output" | grep -oP '(?<=Access key port )\d+')
 
-    # Проверка, что порты успешно извлечены
-    if [[ -z $management_port || -z $access_key_port ]]; then
-        echo "Ошибка при извлечении портов. Проверьте вывод установки:"
-        echo "$install_output"
-        return 1
-    fi
-
-    # Переменная для хранения зеленого цвета и сброса цвета
-    GREEN='\033[0;32m'
-    NC='\033[0m' # No Color
+    # Сохранение API URL и портов в файл конфигурации
+    echo "Сохранение API URL и портов в файл конфигурации..."
+    echo "$api_info" > "$CONFIG_FILE"
+    echo "$management_port" >> "$CONFIG_FILE"
+    echo "$access_key_port" >> "$CONFIG_FILE"
 
     # Вывод информации для пользователя с зелёным цветом для apiUrl
     echo ""
@@ -37,60 +39,21 @@ function install_outline {
     echo -e "${GREEN}${api_info}${NC}"
     read -p "Нажмите Enter для продолжения..."
 
-
     # Настройка брандмауэра для указанных портов
     echo "Открытие портов $management_port (TCP) и $access_key_port (TCP и UDP) в ufw..."
     ufw allow "$management_port/tcp"
     ufw allow "$access_key_port/tcp"
     ufw allow "$access_key_port/udp"
+}
 
-    # Сохранение портов в файл конфигурации
-    echo "Сохранение портов в файл конфигурации..."
-    echo "$management_port" > /root/outline_ports.conf
-    echo "$access_key_port" >> /root/outline_ports.conf
-
-    # Запрос Custom DNS
-    read -p "Введите адрес Custom DNS (например, 94.140.14.14:53): " custom_dns
-
-    echo "Создание DNS скрипта..."
-    cat <<EOT > /root/outlinedns.sh
-#!/bin/bash
-
-# НАПРАВИМ OpenDNS, Cloudflare, Quad9 на Custom DNS
-iptables -t nat -A OUTPUT -d 208.67.222.222/32 -p tcp --dport 53 -j DNAT --to-destination $custom_dns
-iptables -t nat -A OUTPUT -d 208.67.220.220/32 -p tcp --dport 53 -j DNAT --to-destination 1.1.1.1:53
-iptables -t nat -A OUTPUT -d 1.1.1.1/32 -p tcp --dport 53 -j DNAT --to-destination $custom_dns
-iptables -t nat -A OUTPUT -d 9.9.9.9/32 -p tcp --dport 53 -j DNAT --to-destination 1.1.1.1:53
-
-iptables -t nat -A OUTPUT -d 208.67.222.222/32 -p udp --dport 53 -j DNAT --to-destination $custom_dns
-iptables -t nat -A OUTPUT -d 208.67.220.220/32 -p udp --dport 53 -j DNAT --to-destination 1.1.1.1:53
-iptables -t nat -A OUTPUT -d 1.1.1.1/32 -p udp --dport 53 -j DNAT --to-destination $custom_dns
-iptables -t nat -A OUTPUT -d 9.9.9.9/32 -p udp --dport 53 -j DNAT --to-destination 1.1.1.1:53
-EOT
-
-    chmod +x /root/outlinedns.sh
-
-    echo "Создание systemd сервиса для Outline DNS..."
-    cat <<EOT > /etc/systemd/system/outlinedns.service
-[Unit]
-Description=Outline Custom DNS
-After=network-online.target
-
-[Service]
-Type=oneshot
-User=root
-ExecStart=/root/outlinedns.sh
-
-[Install]
-WantedBy=multi-user.target
-EOT
-
-    echo "Запуск и активация Outline DNS..."
-    systemctl daemon-reload
-    systemctl enable outlinedns
-    systemctl start outlinedns
-
-    echo "Outline VPN и Custom DNS установлены и запущены."
+function show_api_url {
+    # Чтение API URL из файла конфигурации
+    if [[ -f $CONFIG_FILE ]]; then
+        api_info=$(sed -n '1p' "$CONFIG_FILE")
+        echo -e "API URL для вашего Outline сервера: ${GREEN}${api_info}${NC}"
+    else
+        echo "API URL недоступен. Сначала установите Outline VPN."
+    fi
 }
 
 function uninstall_outline {
@@ -103,11 +66,12 @@ function uninstall_outline {
     apt autoclean
 
     # Чтение портов из файла конфигурации, если он существует
-    if [[ -f /root/outline_ports.conf ]]; then
+    if [[ -f $CONFIG_FILE ]]; then
         echo "Чтение портов из файла конфигурации..."
-        readarray -t ports < /root/outline_ports.conf
-        management_port=${ports[0]}
-        access_key_port=${ports[1]}
+        readarray -t ports < "$CONFIG_FILE"
+        api_info=${ports[0]}
+        management_port=${ports[1]}
+        access_key_port=${ports[2]}
     else
         echo "Файл конфигурации с портами не найден."
     fi
@@ -137,22 +101,9 @@ function uninstall_outline {
     rm -rf /var/lib/outline
 
     # Удаление файла конфигурации
-    rm -f /root/outline_ports.conf
+    rm -f "$CONFIG_FILE"
 
     echo "Outline и Custom DNS успешно удалены."
-}
-
-function generate_invite_link {
-    read -p "Введите ключ для генерации ссылки-приглашения: " invite_key
-    echo "Ваша ссылка-приглашение: https://s3.amazonaws.com/outline-vpn/invite.html#/ru/invite/$invite_key"
-}
-
-function show_api_url {
-    if [[ -z $api_info ]]; then
-        echo "API URL недоступен. Сначала установите Outline VPN."
-    else
-        echo "API URL для вашего Outline сервера: $api_info"
-    fi
 }
 
 function main_menu {
@@ -162,11 +113,10 @@ function main_menu {
     echo "=============================="
     echo "1. Установка Outline VPN на Ubuntu ARM"
     echo "2. Удаление Outline и Custom DNS"
-    echo "3. Генерация ссылки - приглашения на подключение"
-    echo "4. Отобразить apiUrl"
-    echo "5. Выход"
+    echo "3. Отобразить apiUrl"
+    echo "4. Выход"
     echo "=============================="
-    read -p "Выберите опцию [1-5]: " choice
+    read -p "Выберите опцию [1-4]: " choice
 
     case $choice in
         1)
@@ -176,12 +126,9 @@ function main_menu {
             uninstall_outline
             ;;
         3)
-            generate_invite_link
-            ;;
-        4)
             show_api_url
             ;;
-        5)
+        4)
             exit 0
             ;;
         *)
