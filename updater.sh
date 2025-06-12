1b2180ab90959d577ce87f6676cb777e65d6b827
Joy096 <44851817+Joy096@users.noreply.github.com>
2025-06-12 15:26:41 +0300

    Update updater.sh

--- updater.sh
+++ updater.sh
@@ -4,4 +4,11 @@ apt update
 apt --yes full-upgrade
 yes Y | apt autoremove
 sleep 120  # Задержка в 120 секунд (2 минуты)
+# Removes old revisions of snaps
+# CLOSE ALL SNAPS BEFORE RUNNING THIS
+set -eu
+snap list --all | awk '/disabled/{print $1, $3}' |
+    while read snapname revision; do
+        snap remove "$snapname" --revision="$revision"
+    done
 /sbin/reboot
